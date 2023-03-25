e3f71a185c3d3f947b6e27603b1d00bdbbc1c5e4
Aryan Aman <74642020+AkmAryanAman@users.noreply.github.com>
2023-03-25 20:07:51 +0530

    Update Basics.c

--- Array/Basics.c
+++ Array/Basics.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+int main()
+{
+    int age[5] = {20, 30, 40, 50, 30};   //Array Initialization
+    int ageOfRohit = age[2];             //Accessing Array Element and assign it to ageOfRohit
+    printf("This is 3rd element of age: %d", ageOfRohit);
+  
+    
+    int marks[2];                      //Array Declaration
+    printf("\nPlease Enter Marks in Sub1:");
+    scanf("%d", marks[0]);              //Input value to marks[0] from user
+    printf("\nPlease Enter Marks in Sub1:");
+    scanf("%d", marks[1]);
+    printf("Marks in Sub1: %d", marks[0]);
+    
+    return 0;
+}
